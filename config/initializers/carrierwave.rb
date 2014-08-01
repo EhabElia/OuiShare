83a68bb9d059a0b914fbec9ed3dd676375bd8acd
Rafael Barboza <barboza.cardoso@gmail.com>
2014-08-01 12:54:48 -0300

    Fix spec configs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,19 @@
 begin
   CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider               => 'AWS',       # required
-      :aws_access_key_id      => ENV['aws.key'],       # required
-      :aws_secret_access_key  => ENV['aws.secret'],       # required
-      :region                 => 'eu-west-1',  # optional, defaults to 'us-east-1'
-    }
-    config.fog_directory  = 'ouishare'
+    if Rails.env.production?
+      config.fog_credentials = {
+        :provider               => 'AWS',       # required
+        :aws_access_key_id      => ENV['aws.key'],       # required
+        :aws_secret_access_key  => ENV['aws.secret'],       # required
+        :region                 => 'eu-west-1',  # optional, defaults to 'us-east-1'
+      }
+      config.fog_directory  = 'ouishare'
+    else
+      config.storage = :file
+      if rails.env.test?
+        config.enable_processing = false
+      end
+    end
   end
 rescue Exception => e
   Rails.logger.warn "Error loading settings: #{e}"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,4 +41,21 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.before(:suite) do
+    Geocoder.configure(lookup: :test)
+    Geocoder::Lookup::Test.set_default_stub(
+      [
+        {
+          'latitude'     => 40.7143528,
+          'longitude'    => -74.0059731,
+          'address'      => 'New York, NY, USA',
+          'state'        => 'New York',
+          'state_code'   => 'NY',
+          'country'      => 'United States',
+          'country_code' => 'US'
+        }
+      ]
+    )
+  end
 end
